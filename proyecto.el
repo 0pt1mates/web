a0920e3b6dc09630181177bd2512cfe091ece32b
jm2dev <jomarcar@gmail.com>
2014-05-15 22:53:43 +0100

    Org-mode and jekyll.

--- proyecto.el
+++ proyecto.el
@@ -5,17 +5,19 @@
         ("org-0pt1mates" 
          :base-directory "~/ws-0pt1mates/web/org/"
          :base-extension "org"
-         :publishing-directory "~/ws-0pt1mates/web/public_html/"
+         ;; Path to jekyll project
+         :publishing-directory "~/ws-0pt1mates/web/jekyll/"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4  ; Just the default for this project.
-         :auto-preamble t
+         :html-extension "html"
+         :body-only t ;; Only export section between <body> </body>
          )
 
         ("org-static-0pt1mates"
          :base-directory "~/ws-0pt1mates/web/org/"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/ws-0pt1mates/web/public_html/"
+         :publishing-directory "~/ws-0pt1mates/web/"
          :recursive t
          :publishing-function org-publish-attachment
          )
